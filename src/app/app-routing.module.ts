c93bedabd89938c6fdeae91d4f21b2af1e693b7a
andiwoedwin <andiwoedwin@gmail.com>
2020-11-04 22:50:22 +0300

    add routings to navigate to our pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { WorkComponent } from './work/work.component';
+import { ProgramComponent } from './program/program.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HomepageComponent},
+  {path:"services", component: WorkComponent},
+  {path:"programs", component: ProgramComponent},
+  {path:"contact", component: ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
