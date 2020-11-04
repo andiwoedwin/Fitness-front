927d60ce0766c80764cc44acbe55dc05a612f4ea
andiwoedwin <andiwoedwin@gmail.com>
2020-11-04 22:52:37 +0300

    registering our components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProgramComponent } from './program/program.component';
+// import { ServiceComponent } from './service/service.component';
+import { NavbarComponent } from './navbar/navbar.component';
+// import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    ProgramComponent,
+    // ServiceComponent,
+    NavbarComponent,
+    // ServicesComponent,
+    WorkComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
