2d80e59e3f598725abfe2fc3c19d6f16b4dc6e1a
andiwoedwin <andiwoedwin@gmail.com>
2020-11-05 20:24:44 +0300

    modify our home and program page

---
+++ src/app/gym-service/gym.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
+
+// const AUTH_API 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GymService {
+
+  constructor() { }
+}
